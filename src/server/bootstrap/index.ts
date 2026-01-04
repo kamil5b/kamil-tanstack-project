c5166055b2b7c089e56deb7e20829f8d2504f96e
kamil5b <zee312kamilerz@gmail.com>
2026-01-04 20:29:51 +0700

    feat: Remove unused auth and demo punk songs files; streamline project structure

--- src/server/bootstrap/index.ts
+++ src/server/bootstrap/index.ts
@@ -4,20 +4,39 @@ import { InMemoryTagRepository } from "../repository/inmemory/tag.repository";
 import { createAuthService } from "../service/auth.service";
 import { createProductService } from "../service/product.service";
 import { createTagService } from "../service/tag.service";
+import { AuthService } from "../interfaces/auth.interface";
+import { ProductService } from "../interfaces/product.interface";
+import { TagService } from "../interfaces/tag.interface";
+
+var authInstance: AuthService | null = null;
+var productInstance: ProductService | null = null;
+var tagInstance: TagService | null = null;
 
 export const injectProductService = () => {
+  if(productInstance) {
+    return productInstance;
+  }
   const productRepo = InMemoryProductRepository;
-  return createProductService(productRepo);
+  productInstance = createProductService(productRepo);
+  return productInstance;
 };
 
 export const injectTagService = () => {
+  if(tagInstance) {
+    return tagInstance;
+  }
   const tagRepo = InMemoryTagRepository;
-  return createTagService(tagRepo);
+  tagInstance = createTagService(tagRepo);
+  return tagInstance;
 };
 
 export const injectAuthService = () => {
+  if(authInstance) {
+    return authInstance;
+  }
   const authRepo = InMemoryAuthRepository;
-  return createAuthService(authRepo);
+  authInstance = createAuthService(authRepo);
+  return authInstance;
 }
 
 export const initInjection = () => {
